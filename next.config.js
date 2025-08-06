b514eea9dfb74237e5a4d353896e3f2abc5bab8f
saifmogaadi12 <aboura.joujou@outlook.es>
2025-08-06 15:40:09 +0100

    buld config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost', 'api.charge-tn.com'], // Add your API domains here
+    domains: ['localhost', 'api.powermaps.tech'], // Add your API domains here
   },
   env: {
     API_URL: process.env.API_URL || 'http://localhost:4000',
